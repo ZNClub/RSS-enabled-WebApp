780e4f433702baa2913d5df010ce2268508ef914
ZNevzz <dsouza.nevil45@gmail.com>
2017-04-15 12:16:19 +0530

    xml parse issue

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
 
 function loadDoc() {
 	console.log("onclick");
-  /*  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      myFunction(this);
-    }
-  };
-  xhttp.open("GET", "https://raw.githubusercontent.com/ZNClub/RSS-enabled-WebApp/master/rss.xml", true);
-  xhttp.send(); 
-   */
-  $.get( "https://raw.githubusercontent.com/ZNClub/RSS-enabled-WebApp/master/rss.xml", function( data ) {
-  $( ".result" ).html( data );
-  alert( "Load was performed." );
-  myFunction(data);
-});
   
+	$.get( "https://raw.githubusercontent.com/ZNClub/RSS-enabled-WebApp/master/rss.xml", function( data ) {
+		//$( ".result" ).html( data );
+		//alert( "Load was performed." );
+		console.log("ajax success");
+		myFunction(data);		
+	});
 }
 function myFunction(xml) {
   var i;
   var xmlDoc = xml.responseXML;
+  console.log("data log:");
+  console.log(xml);
   var table="<tr><th>Artist</th><th>Title</th></tr>";
   var x = xmlDoc.getElementsByTagName("CD");
   for (i = 0; i <x.length; i++) { 
