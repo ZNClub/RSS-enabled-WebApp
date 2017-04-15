20f36554887be670b29ca838c67da6354cb4e2c4
ZNevzz <dsouza.nevil45@gmail.com>
2017-04-15 16:54:15 +0530

    python file to update rss.xml successful with urllib

--- rss_update.py
+++ rss_update.py
@@ -4,11 +4,19 @@ Created on Sat Apr 15 16:38:34 2017
 
 @author: ZNevzz
 """
-
+'''
 import urllib2
 
-file = urllib2.urlopen('')
+file = urllib2.urlopen('https://nevildsouza.wordpress.com/feed')
 
 with open("rss_update.xml","wb") as rss:
     rss.write(file.read())
     
+'''
+
+from urllib.request import urlopen
+
+file = urlopen('https://nevildsouza.wordpress.com/feed')
+with open("rss_update.xml","wb") as rss:
+    rss.write(file.read())
+
